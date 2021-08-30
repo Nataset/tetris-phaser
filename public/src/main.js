a8ce4bd716c1f2f06dc877f1448ba345f0b793bc
Nataset Tanabodee <nataset.t@ku.th>
2021-08-30 14:00:32 +0700

    add rotate

--- public/src/main.js
+++ public/src/main.js
@@ -27,17 +27,15 @@ const game1 = new Phaser.Game({
 function init() {
     this.dcount = 0;
 
-    this.cursors = this.input.keyboard.createCursorKeys();
-
-    this.pieceType = [
+    this.pieceTpye = [
         [0, 0, 0],
         [0, 1, 1],
         [1, 1, 0],
     ];
 
     this.player = {
-        pos: { x: 5, y: 0 },
-        pieceType: this.pieceType,
+        pos: { x: 5, y: 10 },
+        piece: this.pieceTpye,
     };
 
     this.field = (() => {
@@ -57,8 +55,8 @@ function init() {
                 if (col !== 0) {
                     activePiece.push(
                         this.add.rectangle(
-                            (x - 1 + offset.x) * BLOCK_WIDTH + BLOCK_WIDTH / 2,
-                            (y - 1 + offset.y) * BLOCK_HEIGHT + BLOCK_HEIGHT / 2,
+                            (x + offset.x) * BLOCK_WIDTH + BLOCK_WIDTH / 2,
+                            (y + offset.y) * BLOCK_HEIGHT + BLOCK_HEIGHT / 2,
                             BLOCK_WIDTH,
                             BLOCK_HEIGHT,
                             0xffffff,
@@ -87,74 +85,116 @@ function init() {
     };
 
     this.collision = (player, field) => {
-        const piece = player.pieceType;
-        const pos = player.pos;
-        let checkFlag = false;
-        piece.forEach((row, y) => {
-            piece.forEach((col, x) => {
-                console.log(y, x)
-                // if (!field[y + pos.y]) {
-                //     checkFlag = true;
-                // } 
-                if (piece[y][x] !== 0 && (field[y + pos.y] && field[y + pos.y][x + pos.x]) !== 0) {
-                    checkFlag = true;
+        const b = player.piece;
+        const o = player.pos;
+        for (let y = 0; y < b.length; y++) {
+            for (let x = 0; x < b[y].length; x++) {
+                if (b[y][x] !== 0 && (field[y + o.y] && field[y + o.y][x + o.x]) !== 0) {
+                    return true;
                 }
-            });
-        });
-        return checkFlag ? true : false;
+            }
+        }
+        return false;
     };
 
     this.join = (player, field) => {
-        player.pieceType.forEach((row, y) => {
+        player.piece.forEach((row, y) => {
             row.forEach((value, x) => {
                 if (value !== 0) {
-                    field[y + player.pos.y - 1][x + player.pos.x - 1] = value;
+                    field[y + player.pos.y][x + player.pos.x] = value;
                 }
             });
         });
     };
+
+    this.move = (Piece, { x, y }) => {
+        Piece.forEach(block => {
+            block.x += BLOCK_WIDTH * x;
+            block.y += BLOCK_HEIGHT * y;
+        });
+    };
+
+    this.collisionBottomHandle = (player, field) => {
+        player.pos.y--;
+        this.join(player, field);
+        player.pos.y = -1;
+        return this.drawPiece(player.piece, player.pos);
+    };
+
+    this.rotateMatrix = (N,mat) =>
+    {
+     
+        for (let x = 0; x < N / 2; x++)
+        {
+            for (let y = x; y < N - x - 1; y++)
+            {
+                let temp = mat[x][y];
+                mat[x][y] = mat[y][N - 1 - x];
+                mat[y][N - 1 - x]
+                    = mat[N - 1 - x][N - 1 - y];
+                mat[N - 1 - x][N - 1 - y] = mat[N - 1 - y][x];
+                mat[N - 1 - y][x] = temp;
+            }
+        }
+    }
+    
+    this.rotate = () => {
+        const piece = this.player.piece
+        this.rotateMatrix(3, piece) 
+        console.log(piece)
+        this.activePiece.forEach(block => {
+            block.destroy();
+        })
+        this.activePiece = this.drawPiece(this.player.piece, this.player.pos)
+
+    };
+
+    this.initInput = () => {
+        this.input.keyboard.on('keydown-LEFT', event => {
+            this.player.pos.x += -1;
+            this.collision(this.player, this.field)
+                ? (this.player.pos.x += 1)
+                : this.move(this.activePiece, { x: -1, y: 0 });
+        });
+
+        this.input.keyboard.on('keydown-RIGHT', event => {
+            this.player.pos.x += 1;
+            this.collision(this.player, this.field)
+                ? (this.player.pos.x += -1)
+                : this.move(this.activePiece, { x: 1, y: 0 });
+        });
+
+        this.input.keyboard.on('keydown-DOWN', event => {
+            this.player.pos.y++;
+            this.collision(this.player, this.field)
+                ? (this.activePiece = this.collisionBottomHandle(this.player, this.field))
+                : this.move(this.activePiece, { x: 0, y: 1 });
+        });
+
+        this.input.keyboard.on('keydown-UP', event => {
+            this.rotate()
+        })
+
+
+    };
+
 }
 
 function preload() {}
 
 function create() {
-    this.activePiece = this.drawPiece(this.player.pieceType, this.player.pos);
+    this.activePiece = this.drawPiece(this.player.piece, this.player.pos);
     this.drawField(this.field);
+    this.initInput();
 }
 
 function update(time, deltaTime) {
     if (this.dcount > 500) {
-        if (!this.collision(this.player, this.field)) {
-            this.player.pos.y++;
-            this.activePiece.forEach(block => {
-                block.y += BLOCK_HEIGHT;
-            });
-        } else {
-            this.join(this.player, this.field);
-            this.player.pos.y = 0;
-            this.activePiece = this.drawPiece(this.player.pieceType, this.player.pos);
-        }
-
-        if (this.dcount > 10) {
-            if (this.cursors.left.isDown) {
-                if (!this.collision(this.player, this.field)) {
-                    console.log(this.player.pos.x);
-                    this.player.pos.x--;
-                    this.activePiece.forEach(block => {
-                        block.x -= BLOCK_WIDTH;
-                    });
-                }
-            } else if (this.cursors.right.isDown) {
-                if (!this.collision(this.player, this.field)) {
-                    console.log(this.player.pos.x);
-                    this.player.pos.x++;
-                    this.activePiece.forEach(block => {
-                        block.x += BLOCK_WIDTH;
-                    });
-                }
-            }
-        }
-
+        window.FIELD = this.field;
+        this.player.pos.y++;
+        this.collision(this.player, this.field)
+            ? (this.activePiece = this.collisionBottomHandle(this.player, this.field))
+            : this.move(this.activePiece, { x: 0, y: 1 });
         this.dcount = 0;
     }
     this.dcount += deltaTime;
